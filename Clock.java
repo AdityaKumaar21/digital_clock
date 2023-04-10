41bf6e40dff42903cb04b2129e3dd42b00f8553f
AdityaKumaar21 <113281443+AdityaKumaar21@users.noreply.github.com>
2023-04-10 16:15:09 +0530

    Add files via upload

---
+++ Clock.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Clock extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
+
+    JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
+    String time;
+    String day;
+    String date;
+    Clock() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Digital Clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 220);
+        this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat=new SimpleDateFormat("EEEE");
+        dateFormat=new SimpleDateFormat("MMMMM dd, yyyy");
+ 
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 59));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setForeground(Color.YELLOW);
+        timeLabel.setOpaque(true);
+
+        dayLabel=new JLabel();
+        dayLabel.setFont(new Font("Verdana",Font.BOLD,34));
+        // dayLabel.setBackground(Color.YELLOW);
+        // dayLabel.setOpaque(true);
+
+        dateLabel=new JLabel();
+        dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
+        // dateLabel.setBackground(Color.YELLOW);
+        // dateLabel.setOpaque(true);
+        ImageIcon icon = new ImageIcon("clock.jpg");
+        this.setIconImage(icon.getImage());
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
+        this.setVisible(true);
+
+        setTimer();
+    }
+
+    public void setTimer() {
+        while (true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new Clock();
+    }
+}
